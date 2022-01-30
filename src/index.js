e736d5c74ce01da6bafe690f6639b1aace157ce1
Carlos Quesada <carlosquesadaumana@gmail.com>
2022-01-30 10:51:15 -0600

    Initial setup fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const app = require('./app');
-const { createRoomTypes } = require('./libs/initialSetup')
+const { 
+      createRoomTypes,
+      createRoles
+} = require('./libs/initialSetup')
 
 createRoomTypes();
+createRoles();
 
 app.listen(app.get('port'), () => {
       console.log("http://localhost:" + app.get('port'))
--- src/libs/initialSetup.js
+++ src/libs/initialSetup.js
@@ -1,4 +1,5 @@
 const RoomType = require("../models/RoomTypes")
+const Role = require("../models/Role")
 
 const createRoomTypes = async () => {
       try {
@@ -18,4 +19,21 @@ const createRoomTypes = async () => {
       }
 };
 
-module.exports = { createRoomTypes }
+const createRoles = async () => {
+      try {
+            const count = await Role.estimatedDocumentCount();
+
+            if (count > 0) return;
+
+            const values = await Promise.all([
+                  new Role({ name: "admin" }).save(),
+                  new Role({ name: "user" }).save()
+            ]);
+
+            console.log(values);
+      } catch (error) {
+            console.log(error)
+      }
+};
+
+module.exports = { createRoomTypes, createRoles }
